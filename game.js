f74a9c71fa5a1342d6ee25f145db06ec0ed46c67
Martin Hipp <martin.hipp@gmail.com>
2014-08-22 10:39:02 +1200

    Updating package.json

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ Game.init = function(sio, socket) {
 	gameSocket.on('hostCreateGame', Game.hostCreateGame);
 	gameSocket.on('hostRoomFull', Game.hostPrepareGame);
 	gameSocket.on('hostCountdownFinished', Game.hostStartGame);
-	gameSocket.on('hostNextRound', Game.hostNextRound);
+	// gameSocket.on('hostNextRound', Game.hostNextRound);
 
 	// Player
 	gameSocket.on('playerJoinGame', Game.playerJoinGame);
@@ -20,19 +20,18 @@ Game.init = function(sio, socket) {
 };
 
 Game.hostCreateGame = function() {
+	var gameId = (Math.random() * 10000) | 0;
 
+	this.emit('newGameCreated', {gameId: gameId, socketId: this.id});
+	this.join(gameId.toString());
 };
 
-Game.hostPrepareGame = function() {
-
+Game.hostPrepareGame = function(gameId) {
+	io.sockets.in(gameId).emit('beginNewGame', {gameId: gameId, socketId: this.id});
 };
 
 Game.hostStartGame = function() {
-
-};
-
-Game.hostNextRound = function() {
-
+	console.log('Start Game');
 };
 
 Game.playerJoinGame = function() {
